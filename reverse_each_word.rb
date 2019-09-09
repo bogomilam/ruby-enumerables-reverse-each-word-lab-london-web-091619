5ad9d47334053777986decdae1e69ab376b3fcbb
bogomilmihaylov <bogomilmihaylov@live.com>
2019-09-09 10:35:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,5 @@
 def reverse_each_word(string)
-  reversed_string = [ ]
-  i = string.length - 1 
-  
-    while i >= 0
-      reversed_string += string[i]
-    i -= 1
-    
+ 
   end
 reversed_string
 end 
