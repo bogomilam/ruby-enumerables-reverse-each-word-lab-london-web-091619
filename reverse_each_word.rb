ba5f6f7dbfb6a1942f7ca465e727a67906e40612
bogomilmihaylov <bogomilmihaylov@live.com>
2019-09-09 10:41:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
 string.split (" ") 
 
-string.each do |string| 
-  puts "#{string.reverse_each_word"
+  string.each do |string| 
+  puts "#{string.reverse_each_word}"
+end
+  puts string
 
 end 
